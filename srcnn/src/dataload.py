ca90c3cb7928fe830e08c413776af32af2b9c7d6
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-09-02 17:09:53 +0530

    dataloading'

--- srcnn/src/dataload.py
+++ srcnn/src/dataload.py
@@ -1,37 +1,31 @@
 import os
-import numpy as np
 from PIL import Image
-import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 class Data(Dataset):
-    def __init__(self, root_dir, transform = None):
-        
+    def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
         
-        self.high_res = os.path.join(self.root_dir, 'high_res')
-        self.low_res = os.path.join(self.root_dir, 'low_res')
+        self.high_res_dir = os.path.join(self.root_dir, 'high_res')
+        self.low_res_dir = os.path.join(self.root_dir, 'low_res')
         
-        if self.transform :
-            self.high_res = self.transform(self.high_res)
-            self.low_res = self.transform(self.low_res)
+        self.image_files = os.listdir(self.high_res_dir)
         
     def __len__(self):
-        return len(self.high_res)
+        return len(self.image_files)
     
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
+        img_name = self.image_files[idx]
         
-        high_res_path = os.path.join(self.high_res, self.high_res[idx])
-        low_res_path = os.path.join(self.low_res, self.low_res[idx])
+        high_res_path = os.path.join(self.high_res_dir, img_name)
+        low_res_path = os.path.join(self.low_res_dir, img_name)
         
-        high_res_images = Image.open(high_res_path).convert('RGB')
-        low_res_images = Image.open(low_res_path).convert('RGB')
+        high_res_image = Image.open(high_res_path).convert("RGB")
+        low_res_image = Image.open(low_res_path).convert("RGB")
         
-        if self.transform :
-            high_res_images = self.transform(high_res_images)
-            low_res_images = self.transform(low_res_images)
+        if self.transform:
+            high_res_image = self.transform(high_res_image)
+            low_res_image = self.transform(low_res_image)
             
-        
+        return low_res_image, high_res_image
