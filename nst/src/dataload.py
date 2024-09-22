ec6db36b7f45dc2a2ef58f014c453b4072ffc516
0xD4rky <ishaan.kumar281@gmail.com>
2024-09-22 15:18:32 +0530

    transform

--- nst/src/dataload.py
+++ nst/src/dataload.py
@@ -13,5 +13,27 @@ import matplotlib.pyplot as plt
 import copy
 from PIL import Image
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.set_default_device(device) # setting default device as gpu
+
+
 img_size = 256 if torch.cuda.is_available() else 128
+
+loader = transforms.Compose([
+    transforms.Resize((img_size,img_size)),
+    transforms.ToTensor()
+])
+
+def img_loader(img_path):
+    img = Image.open(img_path)
+    img = loader(img).unsqueeze(0)
+    return img.to(torch.device("cpu"), torch.float)
+
+
+content = img_loader(r'/home/dell/Documents/cv_stack/nst/data/content.jpg')
+art = img_loader(r'/home/dell/Documents/cv_stack/nst/data/art.jpeg')
+
+assert art.size() == content.size() 
+# assert is a debugging tool in python, raises an Assertion error if condition is not matched'
+
 
