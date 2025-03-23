9a37519582ffc034f7a1d7f737de409b0f77c553
0xD4rky <ishaan.kumar281@gmail.com>
2025-03-23 23:09:56 +0530

    search func implemented

--- src/embeddings.py
+++ src/embeddings.py
@@ -7,6 +7,46 @@ from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 from torch.nn.functional import cosine_similarity
 from serpapi import GoogleSearch
+from dotenv import load_dotenv
 
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+load_dotenv()
+SERPAPI_KEY = os.environ.get("SERPAPI_KEY")
+
+
+def fetch_images_from_google(query, num_images = 3):
+
+    """
+    ROLE: 
+
+        This function is mainly used to search images from google using the SERPAPI based on the query provided by the user
+
+    ARGS:
+
+        query -> query given by the user so search the video
+        num_images -> the number of images to be searched and retrieved from google
+
+    RETURNS:
+
+        a list of tuple containing urls and the pixel arrays of the retrieved images
+    """
+
+    search = GoogleSearch({
+        "q": query,
+        "tbm": "isch",
+        "num": num_images,
+        "api_key": SERPAPI_KEY
+    })
+    results = search.get_dict()
+    image_urls = [img["original"] for img in results.get("images_results", [])[:num_images]]
+    print("Fetched image URLs:", image_urls)
+    
+    images = []
+    for url in image_urls:
+        response = requests.get(url)
+        if response.status_code == 200:
+            img = Image.open(BytesIO(response.content)).convert("RGB")
+            images.append((url, img))
+    return images
