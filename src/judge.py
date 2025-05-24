cce3222e347a076f80940b61269b98c65fefb49c
0xD4rky <ishaan.kumar281@gmail.com>
2025-05-24 21:28:01 +0530

    bug fix

--- src/judge.py
+++ src/judge.py
@@ -110,7 +110,7 @@ class VideoJudge:
         with torch.no_grad(): 
             generated_ids = self.model.generate(**inputs, max_new_tokens=512, do_sample=True, temperature=0.1)
             generated_ids_trimmed = [
-                out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+                out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs["input_ids"], generated_ids)
             ]
             output_text = self.processor.batch_decode(
                 generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
